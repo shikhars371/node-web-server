dcf3edd6dd8ac79de9c66ca764f14286483e1e45
Shikhar <shikhar@Shikhars-MacBook-Air.local>
2017-05-11 01:30:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/public/partials')
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurretYear',()=>
+{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('toupper',(text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/',(request,response)=>{
+
+    //response.send('<h1>hello shikhar </h1>');
+    response.render('home_page.hbs',{
+        
+        pageTitle: 'Welcome to my Site',
+        Help : 'Helping you for your future',
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+         
+        pageTitle:'About Page',
+    });
+});
+
+app.get('/bad',(req,res)=>{
+
+    res.send(
+        {
+            error : 'unable to response'
+        }
+    );
+
+});
+
+app.listen(3000,()=>{
+
+    console.log('server up on 3000');
+
+});
